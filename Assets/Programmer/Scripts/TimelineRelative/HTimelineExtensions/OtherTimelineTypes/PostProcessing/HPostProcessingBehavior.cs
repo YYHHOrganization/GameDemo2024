921a60ab4979927cc19a48edaa43fc19204d7fa4
hhlovesyy <1596944152@qq.com>
2024-03-07 20:23:01 +0800

    add mix logic to global volume post processing

--- Assets/Programmer/Scripts/TimelineRelative/HTimelineExtensions/OtherTimelineTypes/PostProcessing/HPostProcessingBehavior.cs
+++ Assets/Programmer/Scripts/TimelineRelative/HTimelineExtensions/OtherTimelineTypes/PostProcessing/HPostProcessingBehavior.cs
@@ -14,8 +14,11 @@ public enum EnumHPostProcessingType
 public class HPostProcessingBehavior : PlayableBehaviour
 {
     //enum PostprocessingType
-    public List<string> attributes; //后处理效果当中的参数
-    public List<float> values; //后处理效果当中的参数值
+    public string globalVolumeField; //要修改到GlobalVolume的哪个属性
+    public List<string> attributes; 
+    public List<float> values; //同上，只不过是参数值
+    public List<float> defaultValues;
+    public List<int> shouldLerp; //也通过策划表读取，用来判断在mix的时候是否有必要渐变，1表示需要，0表示不需要
     public EnumHPostProcessingType postProcessingType;
     
     // public override void ProcessFrame(Playable playable, FrameData info, object playerData)
@@ -24,23 +27,23 @@ public class HPostProcessingBehavior : PlayableBehaviour
     // }
 
     //实测应该是当timeline播放到这个clip的时候，应该去调用对应的函数
-    public override void OnBehaviourPlay(Playable playable, FrameData info)
-    {
-        Debug.Log("OnBehaviorPlay");
-        if (postProcessingType == EnumHPostProcessingType.GlobalVolume)
-        {
-            HPostProcessingFilters.Instance.SetAttributeAndValueFromTimeline(attributes, values);
-        }
-        
-    }
-
-    //实测应该是当timeline没有播放该clip的时候，应该去调用对应的函数，只会在改变状态的时候调用一次，所以问题不大
+    // public override void OnBehaviourPlay(Playable playable, FrameData info)
+    // {
+    //     Debug.Log("OnBehaviorPlay");
+    //     if (postProcessingType == EnumHPostProcessingType.GlobalVolume)
+    //     {
+    //         HPostProcessingFilters.Instance.SetAttributeAndValueFromTimeline(attributes, values);
+    //     }
+    //     
+    // }
+    //
+    // //实测应该是当timeline没有播放该clip的时候，应该去调用对应的函数，只会在改变状态的时候调用一次，所以问题不大
     public override void OnBehaviourPause(Playable playable, FrameData info)
     {
         Debug.Log("OnBehaviorPause");
         if (postProcessingType == EnumHPostProcessingType.GlobalVolume)
         {
-            HPostProcessingFilters.Instance.ResetAttributesAndVolumes();
+            HPostProcessingFilters.Instance.ResetInput(this);
         }
     }
 }
