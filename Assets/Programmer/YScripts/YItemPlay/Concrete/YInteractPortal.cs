eb8419504bab98e715cecc87bd02df513d07f5ef
hhlovesyy <1596944152@qq.com>
2024-03-26 15:59:23 +0800

    add portal logic, add pamu and heita character

--- Assets/Programmer/YScripts/YItemPlay/Concrete/YInteractPortal.cs
+++ Assets/Programmer/YScripts/YItemPlay/Concrete/YInteractPortal.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class YInteractPortal : YIInteractiveGroup
 {
-
+    private string needId = "20000009";
+    private int needCount = 3;
+    HItemShowPanel panel = new HItemShowPanel();
+    private bool isPushed;
+    private int actualCount;
     public void Start()
     {
         base.Start();
@@ -16,11 +22,38 @@ public class YInteractPortal : YIInteractiveGroup
         
         //如果是靠近门打开 只会有一个trigger
         YTriggerEvents.RaiseOnShortcutKeyInteractionStateChanged(false, triggers[0],null);
-        
+        StartCoroutine(ShowNeedItemPanel());
+
     }
+
+    IEnumerator ShowNeedItemPanel()
+    {
+        YGameRoot.Instance.Push(panel);
+        YTriggerEvents.RaiseOnMouseLockStateChanged(false);
+        YPlayModeController.Instance.LockPlayerInput(true);
+        
+        isPushed = true;
+        panel.SetMiddlePanelActive(false);
+        panel.SetLeftScrollPanelActive(false);
+        panel.SetGiveOutPanelActive(true);
+        
+        var item = yPlanningTable.Instance.worldItems[needId];
+        AsyncOperationHandle<Sprite> handle =
+            Addressables.LoadAssetAsync<Sprite>(item.UIIconLink);
+        yield return handle;
+        actualCount = HItemCounter.Instance.CheckCountWithItemId(needId);
+        panel.ShowGiveOutItems(handle.Result, needCount, actualCount, this);
+    }
+
+
+    public bool CheckCountIsRightOrNot()
+    {
+        return (needCount==actualCount);
+    }
+    
     public override void SetResultOff()
     {
-        
+       
     }
     
     public override void EnterField(bool isEnter, GameObject TriggergameObject,Transform showUIPlace)
@@ -32,7 +65,7 @@ public class YInteractPortal : YIInteractiveGroup
         }
         else
         {
-            Debug.Log("离开传送门区域");
+            StopAllCoroutines();
         }
     
     }
