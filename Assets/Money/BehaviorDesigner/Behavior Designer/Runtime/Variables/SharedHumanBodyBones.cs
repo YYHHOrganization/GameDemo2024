9f08e7d853405aabb9c9c1e82fbe775b4bc869f0
AresFox <2955984049@qq.com>
2024-06-29 15:13:00 +0800

    delete useless

--- Assets/Money/BehaviorDesigner/Behavior Designer/Runtime/Variables/SharedHumanBodyBones.cs
+++
@@ -1,8 +0,0 @@
-namespace BehaviorDesigner.Runtime
-{
-    [System.Serializable]
-    public class SharedHumanBodyBones : SharedVariable<UnityEngine.HumanBodyBones>
-    {
-        public static implicit operator SharedHumanBodyBones(UnityEngine.HumanBodyBones value) { return new SharedHumanBodyBones { Value = value }; }
-    }
-}
