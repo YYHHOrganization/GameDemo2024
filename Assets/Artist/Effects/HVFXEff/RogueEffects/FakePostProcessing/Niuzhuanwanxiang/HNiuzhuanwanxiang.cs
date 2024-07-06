0d0d7fd9abdcb472fdd605dc7e483e6ee03cf59b
hhlovesyy <1596944152@qq.com>
2024-07-06 16:45:56 +0800

    添加一些与扭转万象道具有关的基础逻辑

---
+++ Assets/Artist/Effects/HVFXEff/RogueEffects/FakePostProcessing/Niuzhuanwanxiang/HNiuzhuanwanxiang.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HNiuzhuanwanxiang : MonoBehaviour
+{
+    public RenderTexture renderTexture;
+
+    private Camera playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerCamera
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
