457d31d670c6f0c2d244cbb3924899c7b731afc7
hhlovesyy <1596944152@qq.com>
2024-10-10 21:04:54 +0800

    添加了两个道具

---
+++ Assets/Programmer/Scripts/HScripts/HGamePlay/HRogueLikeGame/BaseLogic/RogueItemLogics/ChangeTimeScaleItem.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ChangeTimeScaleItem : HRogueItemBase
+{
+    protected override void UseNegativeItem(string funcName, string funcParams)
+    {
+        if (funcName != "UseInSelfFunction")  //应该是填错了，此时报错
+        {
+            Console.Error.WriteLine("UseNegativeItem: funcName is not UseInSelfFunction");
+            return;
+        }
+        string[] param = funcParams.Split(';');
+        float timeScale = float.Parse(param[0]);
+        float duration = float.Parse(param[1]);
+        HRogueItemFuncUtility.Instance.SetTimeScaleTime(timeScale);
+        DOVirtual.DelayedCall(duration, () =>
+        {
+            HRogueItemFuncUtility.Instance.ResetTimeScale();
+        });
+    }
+}
