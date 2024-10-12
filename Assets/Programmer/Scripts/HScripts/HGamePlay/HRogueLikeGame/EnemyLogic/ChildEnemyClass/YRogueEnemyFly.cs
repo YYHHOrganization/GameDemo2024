e3eb8d5587f4fd7397571c1e79eec255f320f78d
AresFox <2955984049@qq.com>
2024-10-12 18:50:13 +0800

    增加飞天怪物 可抛物体包括可时间回溯的炸药等

---
+++ Assets/Programmer/Scripts/HScripts/HGamePlay/HRogueLikeGame/EnemyLogic/ChildEnemyClass/YRogueEnemyFly.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class YRogueEnemyFly : HRogueEnemyPatrolAI
+{
+    // InitStateMachine()
+    protected override void InitStateMachine()
+    {
+        var states = new Dictionary<Type, HRogueEnemyBaseState>
+        {
+            {typeof(YRogueEnemyFlyState), new YRogueEnemyFlyState(this) },
+            {typeof(YRogueEnemyThrowState), new YRogueEnemyThrowState(this)},
+            {typeof(HRogueEnemyCommonBeFrozenState), new HRogueEnemyCommonBeFrozenState(this)}
+        };
+        GetComponent<HRogueEnemyCommonStateMachine>().SetStates(states);
+    }
+
+    public override void Fly()
+    {
+        //将自身的位置设置到飞行高度
+        transform.position = new Vector3(transform.position.x, flyheight, transform.position.z);
+        StartCoroutine(FlyCoroutine());
+    }
+    
+    float flyheight = 2f;
+
+    IEnumerator FlyCoroutine()
+    {
+        Debug.Log("EnemyMoveRandomly");
+        while (true)
+        {
+            float moveTime = Random.Range(0, wanderMaxMoveTime);
+            float moveDistance = enemy._RogueEnemyWanderSpeed() * moveTime;
+            int count = 0; //开一个计数器，防止怪物卡死
+            while (isMoving)
+            {
+                Vector3 moveDirection = new Vector3(Random.Range(-1f, 1f)* moveDistance,  0, Random.Range(-1f, 1f)*moveDistance) ;
+                Vector3 destination = transform.position + moveDirection;
+                //怪物要转向前进的目标
+                transform.LookAt(destination);
+                animator.SetBool(isWalkingHash, true);
+                
+                while (transform.position != destination && isMoving)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * enemy._RogueEnemyWanderSpeed());
+                    count++;
+                    if (count == 100)
+                    {
+                        count = 0;
+                        isMoving = false;
+                    }
+                    yield return null;
+                }
+                isMoving = false;
+            }
+
+            if (!isMoving)
+            {
+                animator.SetBool(isWalkingHash, false);
+                float stopTime = Random.Range(0, wanderMaxStopTime);
+                yield return new WaitForSeconds(stopTime);
+                isMoving = true;
+            }
+        }
+    }
+
+}
---
+++ Assets/Programmer/Scripts/HScripts/HGamePlay/HRogueLikeGame/EnemyLogic/YRogueEnemyThrowState.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Random = UnityEngine.Random;
+
+public class YRogueEnemyThrowState : HRogueEnemyBaseState
+{
+    private HRogueEnemyPatrolAI mPatrolAI;
+    public YRogueEnemyThrowState(HRogueEnemyPatrolAI patrolAI) : base(patrolAI.gameObject)
+    {
+        mPatrolAI = patrolAI;
+    }
+    
+    public override Type Tick()
+    {
+        if (mPatrolAI.isDead)
+            return null;
+        // if(mPatrolAI.mNavMeshAgent == null || mPatrolAI.mNavMeshAgent.enabled == false)
+        //     return null;
+        // mPatrolAI.mNavMeshAgent.destination = mPatrolAI.mTarget.position;
+        
+        // /transform.LookAt(patrolAI.mTarget);
+        // //transform.Translate(Vector3.forward*Time.deltaTime*YGameSetting.PatrolAISpeed);
+        //
+        // if (mPatrolAI.CheckIfTargetIsNear(mPatrolAI.attackRange))
+        // {
+        //     mPatrolAI.animator.SetBool(mPatrolAI.IsAttackingHash, true);
+        //     //return typeof(YAttackState);
+        // }
+        //
+        // if (mPatrolAI.isDead == true)
+        // {
+        //     //不再追踪
+        //     mPatrolAI.mNavMeshAgent.enabled = false;
+        //     // patrolAI.mNavMeshAgent.ResetPath();
+        //     return typeof(HRogueEnemyCommonDieState);
+        // }
+        return null;
+    }
+
+    private void SetNavmeshDestination()
+    {
+        if (mPatrolAI.mTarget)
+        {
+            if (mPatrolAI)
+            {
+                mPatrolAI.mNavMeshAgent.destination = mPatrolAI.mTarget.position;
+                //trick:感觉这里给敌人设置不同的速度等参数会更好，不然召唤出来之后他们的逻辑都是完全一样的
+                mPatrolAI.mNavMeshAgent.speed = Random.Range(1, mPatrolAI.enemy._RogueEnemyChaseMaxSpeed());
+                mPatrolAI.mNavMeshAgent.acceleration = Random.Range(2, mPatrolAI.chaseMaxAcceleration);
+            }
+        }
+    }
+    
+    //enter
+    public override void OnStateEnter()
+    {
+        
+        // return;
+        if (mPatrolAI.chaseType == RogueEnemyChaseType.DontMove)
+        {
+            return;
+        }
+        if (mPatrolAI.chaseType == RogueEnemyChaseType.ShootSpecialMuzzleDontMove)
+        {
+            mPatrolAI.StartCoroutine(mPatrolAI.ShootSpecialBulletWithMuzzle());
+            return;
+        }
+
+        if (mPatrolAI.mNavMeshAgent == null)
+        {
+            return;
+        }
+        
+        mPatrolAI.mNavMeshAgent.enabled = true;
+        mPatrolAI.curStateName = "chase";
+        
+        //根据chase类型决定接下来的逻辑
+        if (mPatrolAI.chaseType == RogueEnemyChaseType.JustChase)
+        {
+            SetNavmeshDestination();
+        }
+        else if (mPatrolAI.chaseType == RogueEnemyChaseType.ChaseAndShootPlayer)
+        {
+            SetNavmeshDestination();
+            mPatrolAI.ShootBulletForward(true,true);
+        }
+        else if (mPatrolAI.chaseType == RogueEnemyChaseType.ChaseAndShootSpecial)
+        {
+            string funcName = mPatrolAI.enemy.RogueEnemyChaseShootFunc;
+            //startCorotine
+            mPatrolAI.StartCoroutine(funcName);
+        }
+        else if (mPatrolAI.chaseType == RogueEnemyChaseType.ChaseAndShootWithSpecialMuzzle)
+        {
+            Debug.Log("ChaseAndShootWithSpecialMuzzle");
+            //进到这里的逻辑都是直接Instantiate对应的muzzle，然后间隔一段时间shoot一次
+            //string funcName = mPatrolAI.enemy.RogueEnemyChaseShootFunc; //e.g.ShootBulletMuzzleSpiral
+            //GameObject muzzle = Addressables.LoadAssetAsync<GameObject>(funcName).WaitForCompletion();
+            mPatrolAI.StartCoroutine(mPatrolAI.ShootSpecialBulletWithMuzzle());
+        }
+        else if (mPatrolAI.chaseType == RogueEnemyChaseType.AddSthToPlayer)
+        {
+            string funcName = mPatrolAI.enemy.RogueEnemyChaseShootFunc;
+            
+        }
+    }
+}
